db5ade542ba1d3043eedb1cdc1862ee7e9989cc3
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-09-30 23:51:50 +0700

    update font verify

--- src/components/CreditCardFormBody.tsx
+++ src/components/CreditCardFormBody.tsx
@@ -6,7 +6,7 @@ export default function CreditCardFormBody() {
   };
   return (
     <form onSubmit={handleSubmit} className='grid gap-y-3'>
-      <CreditCardFormInput placeholder='Full name' type='text' labelName='Full name' />
+      <CreditCardFormInput placeholder='Kenny Omega' type='text' labelName='Full name' />
       <CreditCardFormInput disabled placeholder='1234 1234 1234 1234' type='text' labelName='Credit card number' />
       <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
         <CreditCardFormInput disabled placeholder='MM/YY' type='text' labelName='Exp Date' />
@@ -16,7 +16,7 @@ export default function CreditCardFormBody() {
       <button className='bg-[#288FB4] hover:bg-[#82CCDD] duration-300 text-white font-bold py-2 px-3 rounded w-full capitalize mt-3'>
         Confirm payment
       </button>
-      <p className='text-center text-black'>You verify that this info is correct</p>
+      <p className='text-center text-black text-xs'>You verify that this info is correct</p>
     </form>
   );
 }
