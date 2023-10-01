9dfdece81d9ac3782fdfbcf6f9b98edce805629a
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-01 12:06:25 +0700

    update icon!

--- src/components/CreditCardFormBody.tsx
+++ src/components/CreditCardFormBody.tsx
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CreditCardFormInput from "./CreditCardFormInput";
+import { faCreditCard } from "@fortawesome/free-solid-svg-icons/faCreditCard";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons/faCircleInfo";
 
 export default function CreditCardFormBody() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -7,10 +10,16 @@ export default function CreditCardFormBody() {
   return (
     <form onSubmit={handleSubmit} className='grid gap-y-3'>
       <CreditCardFormInput placeholder='Kenny Omega' type='text' labelName='Full name' />
-      <CreditCardFormInput disabled placeholder='1234 1234 1234 1234' type='text' labelName='Credit card number' />
+      <CreditCardFormInput
+        disabled
+        placeholder='1234 1234 1234 1234'
+        type='text'
+        labelName='Credit card number'
+        icon={<FontAwesomeIcon icon={faCreditCard} />}
+      />
       <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
         <CreditCardFormInput disabled placeholder='MM/YY' type='text' labelName='Exp Date' />
-        <CreditCardFormInput disabled placeholder='•••' type='password' labelName='CVV' />
+        <CreditCardFormInput disabled placeholder='•••' max={3} type='password' labelName='CVV' icon={<FontAwesomeIcon icon={faCircleInfo} />} />
       </div>
       <CreditCardFormInput disabled placeholder='90210' type='text' labelName='Zip code' />
       <button className='bg-[#288FB4] hover:bg-[#82CCDD] duration-300 text-white font-bold py-2 px-3 rounded w-full capitalize mt-3'>
--- src/components/CreditCardFormInput.tsx
+++ src/components/CreditCardFormInput.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type Props = {
   labelName: string;
   placeholder: string;
@@ -5,22 +7,30 @@ type Props = {
   className?: string;
   disabled?: boolean;
   readOnly?: boolean;
+  icon?: React.ReactNode;
+  min?: number;
+  max?: number;
 };
 
 export default function CreditCardFormInput(props: Props) {
   return (
     <div className={`w-full ${props.className} mx-auto`}>
-      <label className='space-y-1'>
+      <label className='space-y-1 relative'>
         <div className='capitalize'>{props.labelName}</div>
         <input
+          minLength={props.min || 1}
+          maxLength={props.max}
           disabled={props.disabled}
           readOnly={props.readOnly}
-          className={`border-2 border-gray-300 focus:border-gray-700 outline-none px-3 py-1 rounded-lg shadow-2xl w-full ${
-            props.disabled ? "bg-gray-300" : "bg-white"
-          }`}
+          className={`border-2 border-gray-300 focus:border-gray-700 outline-none px-3 py-1 ${
+            props.icon ? "pr-9" : ""
+          } rounded-lg shadow-2xl w-full ${props.disabled ? "bg-gray-300" : "bg-white"} duration-300`}
           type={props.type || "text"}
           placeholder={props.placeholder}
         />
+        {props.icon && (
+          <div className='absolute inset-y-0 top-[32px] text-gray-500 right-0 pr-3 flex items-center pointer-events-none'>{props.icon}</div>
+        )}
       </label>
     </div>
   );
